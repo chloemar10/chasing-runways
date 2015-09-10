153521cc52e61abfa3563735491bf88ab617a653
Jon Haygood <jha@quartetfs.com>
2015-09-09 21:45:58 -0400

    Frame out the carousel

--- js/my.js
+++ js/my.js
@@ -1,5 +1,4 @@
 (function() {
-
   // style the navigation
   var path = /([^\/]*)\.html$/.exec(window.location.pathname)[1];
   $('.navigation a').each(function() {
@@ -12,4 +11,23 @@
       $nav.removeAttr('id');
     }
   });
+
+  // carousel
+  var photos = ['posts/first/1.png', 'posts/first/2.png', 'posts/first/3.png'];
+  var $carList = $('#car-list');
+  photos.forEach(function(photo) {
+    var $li = $('<li/>');
+    var $img = $('<img />', {
+      src: photo,
+      alt: 'MyAlt'
+    });
+    $img.appendTo($li);
+    $li.appendTo($carList);
+  });
+
+  var $img = $('<img />', {
+    src: photos[0],
+    alt: 'MyAlt'
+  });
+  $img.appendTo($('#car-main'));
 })();
